e108c46bf56a6b0d9a6b56b81e008eb059f7e520
Marius Kilian <mkilian96@gmail.com>
2023-11-21 23:14:57 +0000

    Changed system for portfolio URLs to hashes

--- scripts/url-with-modal.js
+++ scripts/url-with-modal.js
@@ -1,16 +1,15 @@
-const urlParams = new URLSearchParams(window.location.search);
-const portfolioToOpen = urlParams.get("portfolio");
+const portfolioToOpen = window.location.hash.substring(1).split("?")[0];
+console.log(window.location);
 
-if (portfolioToOpen) {
+if (portfolioToOpen !== null && portfolioToOpen !== "") {
   var modalLink = document.getElementById("portfolioLink" + portfolioToOpen);
   if (modalLink) setTimeout(() => modalLink.click(), 250);
 }
-
+// When clicking a portfolio item on the page, this makes sure the url is appended
 function appendModalUrl(modalId) {
-  const newUrl = window.location.href.split("?")[0] + `?portfolio=${modalId}`;
+  const newUrl = window.location.origin + `/#${modalId}`;
   history.pushState({ modal: modalId }, null, newUrl);
 }
-
 var portfolioLinks = document.getElementsByClassName("portfolio-link");
 for (let i = 0; i < portfolioLinks.length; i++) {
   let link = portfolioLinks[i];
@@ -20,7 +19,5 @@ for (let i = 0; i < portfolioLinks.length; i++) {
 }
 
 $(".portfolio-modal").on("hide.bs.modal", function () {
-  const currentUrl = new URL(window.location.href);
-  currentUrl.searchParams.delete("portfolio");
-  history.replaceState({}, null, currentUrl.toString());
+  history.replaceState(null, null, window.location.pathname);
 });
