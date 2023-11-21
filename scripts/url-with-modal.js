e0fe2120552cce2424601c729414789e8bca7ee5
Marius Kilian <mkilian96@gmail.com>
2023-11-21 23:21:19 +0000

    changed from "/#Project" to "/#/Project"

--- scripts/url-with-modal.js
+++ scripts/url-with-modal.js
@@ -1,4 +1,4 @@
-const portfolioToOpen = window.location.hash.substring(1).split("?")[0];
+const portfolioToOpen = window.location.hash.substring(2).split("?")[0];
 console.log(window.location);
 
 if (portfolioToOpen !== null && portfolioToOpen !== "") {
@@ -7,7 +7,7 @@ if (portfolioToOpen !== null && portfolioToOpen !== "") {
 }
 // When clicking a portfolio item on the page, this makes sure the url is appended
 function appendModalUrl(modalId) {
-  const newUrl = window.location.origin + `/#${modalId}`;
+  const newUrl = window.location.origin + `/#/${modalId}`;
   history.pushState({ modal: modalId }, null, newUrl);
 }
 var portfolioLinks = document.getElementsByClassName("portfolio-link");
